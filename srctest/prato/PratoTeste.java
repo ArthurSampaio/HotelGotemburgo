60c44a340db89265d2a7d30a558a727474508c4d
ArthurSampaio <arthur.sampaio.correia@ccc.ufcg.edu.br>
2016-09-16 09:19:00 -0300

    testes para as Classes do tipo Prato

---
+++ srctest/prato/PratoTeste.java
@@ -0,0 +1,56 @@
+package prato;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import prato.Prato;
+
+public class PratoTeste {
+
+	@Test
+	public void criaPrato() {
+
+		try {
+
+			Prato prato3 = new Prato("nome3", "descricao3", 23.40);
+			Prato prato4 = new Prato("nome4", "descricao4", 34.60);
+
+		} catch (Exception e) {
+			fail(); /* Nao deve ser lancada exececao */
+
+		}
+
+	}
+
+	@Test
+	public void criaPratoException() {
+		try {
+			Prato prato4 = new Prato("", "descricao4", 67.0);
+			Prato prato5 = new Prato(null, "descricao5", 34.0);
+			fail();
+		} catch (Exception e) {
+			assertEquals("String nula ou vazia", e.getMessage());
+
+		}
+
+		try {
+			Prato prato4 = new Prato("nome4", "", 67.0);
+			Prato prato5 = new Prato("nome5", null, 34.0);
+			fail();
+
+		} catch (Exception e) {
+			assertEquals("String nula ou vazia", e.getMessage());
+		}
+
+		try {
+			Prato prato4 = new Prato("nome4", "descricao4", -67.0);
+			fail();
+		} catch (Exception e) {
+			assertEquals("Valor inv�lido", e.getMessage());
+		}
+
+	}
+
+}
