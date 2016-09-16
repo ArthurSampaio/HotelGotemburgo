c64d3ba956ebe3e08b22321a359afbfe560d0582
ArthurSampaio <arthur.sampaio.correia@ccc.ufcg.edu.br>
2016-09-16 09:18:09 -0300

    Implementada o cliente factory

---
+++ src/cliente/ClienteFactory.java
@@ -0,0 +1,39 @@
+package cliente;
+
+/**
+ * Factory da Classe Cliente
+ * 
+ * @author Arthur Sampaio
+ *
+ */
+public class ClienteFactory {
+	
+	
+	public ClienteFactory(){}
+	
+	/**
+	 * Cria um objeto cliente
+	 * 
+	 * @param nome
+	 *            O nome do cliente
+	 * @param dataNasc
+	 *            A data de nascimento do cliente
+	 * @param email
+	 *            O Email do cliente
+	 * @param hosp
+	 *            Representa se esta hospedado ou nao
+	 * @throws Exception
+	 *             Quando alguma das strings é invalida.
+	 */
+	public Cliente criaCliente(String nome, String email, String dataNasc, boolean hosp) throws Exception {
+		return new Cliente( nome, email, dataNasc,hosp);
+	}
+	
+	/**
+	 * Cria um cliente quando o cliente nao estiver hospedado
+	 */
+	public Cliente criaCliente(String nome, String email, String dataNasc) throws Exception{
+		return new Cliente(nome,email,dataNasc);
+	}
+
+}
